a437a5bccb138e42f852a050cea6f0d84130c190
Seun Matt <smatt382@gail.com>
2021-11-21 17:11:30 +0100

    implemented parent pom structure

---
+++ README.md
@@ -0,0 +1,3 @@
+# SmattMe Tutorials
+
+This is the base repository for all the sample code used in the technical articles published on [smattme.com](https://smattme.com)
---
+++ monolith-spring-boot-exception-handling/src/test/java/com/smattme/monolithspringbootexceptionhandling/MonolithSpringBootExceptionHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smattme.monolithspringbootexceptionhandling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonolithSpringBootExceptionHandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
