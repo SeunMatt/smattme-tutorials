aefcf0d4d08b4f4866cc4144a8904c6c6c14f14b
Seun Matt <smatt382@gmail.com>
2024-10-06 18:23:59 +0100

    added sample code spring boot intro

---
+++ spring-boot-intro/src/test/java/com/smattme/springbootintro/SpringBootIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smattme.springbootintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
